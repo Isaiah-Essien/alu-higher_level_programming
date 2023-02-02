b82c741ddcd76122d05049e07526594fafdad3ca
Isaiah-Essien <i.essien@alustudent.com>
2023-02-02 07:09:48 -0800

    100-write.py

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korp r, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpr, 2015-10-19\n")
 sys.exit(1)
