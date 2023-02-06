0bb424aff818427929c0a76139c3c01c1ed987fd
Isaiah-Essien <i.essien@alustudent.com>
2023-02-06 06:00:42 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for xup in range(ord('a'), ord('z') + 1):
+    if chr(xup) != 'q' and chr(xup) != 'e':
+        print("{}".format(chr(xup)), end="")
