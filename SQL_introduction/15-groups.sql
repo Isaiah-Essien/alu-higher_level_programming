23c9b12e78a0b6de77d14996651364ae01817004
Isaiah-Essien <i.essien@alustudent.com>
2023-03-27 14:51:00 -0700

    7-insert_value.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,2 @@
--- Lists all records of the table second_table having a name value in my MySQL server.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC
+-- group by
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
