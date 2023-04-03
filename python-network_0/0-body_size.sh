ddfd2ddedd20c37c47f0ebc844f8400c2893b664
Isaiah-Essien <i.essien@alustudent.com>
2023-04-03 14:51:11 +0000

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Check if URL argument is provided
-
 [ -z "$1" ] && { echo "Error: Please provide a URL as an argument."; exit 1; }
-
 size=$(curl -s -w "\n%{size_download}\n" -o /dev/null "$1" | tail -n 1)
-
 echo "Size of response body: $size bytes"
