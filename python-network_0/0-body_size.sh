bef204cdf52f0a7ee042e7edf9889385e4b303a0
Isaiah-Essien <i.essien@alustudent.com>
2023-04-03 14:48:27 +0000

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 # Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Error: Please provide a URL as an argument."
-  exit 1
-fi
 
-# Send request using curl and get the size of the response body
-response=$(curl -s -w "\n%{size_download}\n" -o /dev/null "$1")
-size=$(echo "$response" | tail -n 1)
+[ -z "$1" ] && { echo "Error: Please provide a URL as an argument."; exit 1; }
 
-# Display the size of the response body in bytes
+size=$(curl -s -w "\n%{size_download}\n" -o /dev/null "$1" | tail -n 1)
+
 echo "Size of response body: $size bytes"
