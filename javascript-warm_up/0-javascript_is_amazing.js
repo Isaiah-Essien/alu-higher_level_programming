2bb745b51b609ea1461ec3ee974da4e0d38002e8
Isaiah-Essien <i.essien@alustudent.com>
2023-06-06 17:18:39 -0700

    0-javascript_is_amazing.js

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-//This Script prints JavaScript is amazing
+// Script that prints "JavaScript is amazing".
 
-const myVar = “JavaScript is amazing”;
-console.log(myVar;)
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
