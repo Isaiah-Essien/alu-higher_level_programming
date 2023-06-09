9f2b43d7c973e3cebb338f7a26509c6251dae5d5
Isaiah-Essien <i.essien@alustudent.com>
2023-06-09 10:51:04 -0700

    task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if ((w < 1) || (h < 1)) {
-    } else if ((isNaN(w)) || (isNaN(h))) {
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
@@ -11,11 +9,7 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
+      console.log('X'.repeat(this.width));
     }
   }
 };
